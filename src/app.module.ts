9d7670535bc2df89e3dff039358620f8c00ff5a9
Luis Miguel Goulart <luismiguelgoulart@gmail.com>
2024-11-11 09:17:37 -0300

    alterações para usar o docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,23 @@ import { StudentsModule } from './modules/students/students.module';
 import { DisciplinesModule } from './modules/disciplines/disciplines.module';
 import { LessonsModule } from './modules/lessons/lessons.module';
 import { ContentsModule } from './modules/contents/contents.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST || 'postgres',
+      port: parseInt(process.env.PG_PORT, 10) || 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, 
+    }),
+    
     GraphQLModule.forRoot({
       debug: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
